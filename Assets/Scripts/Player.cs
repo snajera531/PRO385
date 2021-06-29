add7345601525c8cef8e28841ca1629e34dca2e6
snajera531 <salemnajera@gmail.com>
2021-06-28 21:53:40 -0600

    changes for controller assignment

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -10,7 +11,7 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-        
+        GetComponent<PlayerInput>().onActionTriggered += HandleAction;
     }
 
     void Update()
@@ -24,10 +25,42 @@ public class Player : MonoBehaviour
         {
             OnFire();
         }
+
+        Gamepad gamepad = Gamepad.current;
+        if (gamepad == null) return;
+        input = gamepad.leftStick.ReadValue();
+
+        if (gamepad.buttonSouth.wasPressedThisFrame)
+        {
+            OnFire();
+        }
     }
 
-    void OnFire()
+    public void OnFire()
     {
         Instantiate(shot, transform.position, Quaternion.identity);
+    }
+
+    public void OnMove(InputValue inputValue)
+    {
+        input = inputValue.Get<Vector2>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
+    }
+
+    private void HandleAction(InputAction.CallbackContext context)
+    {
+        if(context.action.name == "Fire")
+        {
+            OnFire();
+        }
+
+        if(context.action.name == "Move")
+        {
+            OnMove(context);
+        }
     }
 }
